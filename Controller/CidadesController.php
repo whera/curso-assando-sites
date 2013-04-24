6616a9511b91d9b011f83697beaea69d2df7f1db
Ronaldo M. Rodrigues <ronaldo@whera.com.br>
2013-04-24 18:12:22 -0300

    Adicionar action index

--- Controller/CidadesController.php
+++ Controller/CidadesController.php
@@ -5,6 +5,15 @@ class CidadesController extends AppController {
 
 	public function index() {
 
+		//print_r($this->Cidade->PegaCidades());
+
+		$cidades = $this->Cidade->PegaCidades();
+		if( count($cidades) > 0 ){
+			$this->set('cidades', $cidades);
+		}
+		else{
+			$this->redirect('/');
+		}
 	}
 }
 
