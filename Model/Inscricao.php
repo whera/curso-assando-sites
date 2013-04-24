11c26687474e08774e6dc4c5361082ff4e9b6e9c
Ronaldo M. Rodrigues <ronaldo@whera.com.br>
2013-04-24 16:49:35 -0300

    Adicionando validação no formulario

--- Model/Inscricao.php
+++ Model/Inscricao.php
@@ -5,6 +5,52 @@ class Inscricao extends AppModel {
 	public $displayField = 'nome';
 	public $order        = array('nome' => 'ASC');
 
+	public $validate = array(
+        'nome' => array(
+            array(
+                'rule' => 'notEmpty',
+                'message' => 'Você deve preencher o Nome.',
+            ),
+        ),
+        'email' => array(
+            array(
+                'rule' => 'notEmpty',
+                'message' => 'Você deve preencher o Email.',
+            ),
+            array(
+                'rule' => 'email',
+                'message' => 'Você deve preencher um email válido.',
+            ),
+            array(
+                'rule' => 'isUnique',
+                'message' => 'Este email já existe em nosso cadastro.',
+            ),
+        ),
+        'telefone' => array(
+            array(
+                'rule' => 'notEmpty',
+                'message' => 'Você deve preencher o Telefone.',
+            ),
+            array(
+                'rule' => 'naturalNumber',
+                'message' => 'Somente são aceitos números para este campo.',
+            ),
+        ),
+        'endereco' => array(
+            array(
+                'rule' => 'notEmpty',
+                'message' => 'Você deve preencher o Endereço.',
+            ),
+            array(
+                'rule' => array('minLength', 10),
+                'message' => 'Você deve informar no mínimo 10 caracteres para o endereço.',
+            ),
+        )
+    );
+
+
+
+
 	public function estados(){
 		return array(
 			"AC"=>"Acre",
