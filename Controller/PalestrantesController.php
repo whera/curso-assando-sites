6702fdae1005021394ba5e2c229699db2f679299
Ronaldo M. Rodrigues <ronaldo@whera.com.br>
2013-04-26 16:11:13 -0300

    Action Index pega os valores do model e envia para View

--- Controller/PalestrantesController.php
+++ Controller/PalestrantesController.php
@@ -2,10 +2,19 @@
 App::uses('AppController', 'Controller');
 
 class PalestrantesController extends AppController {
-	
+
 	public $helpers = array('Whera');
 
 	public function index(){
+
+		$palestrantes = $this->Palestrante->ListaPalestrantes();
+
+		if($palestrantes){
+			$this->set('palestrantes', $palestrantes);
+		}
+		else{
+			$this->redirect('/');
+		}
 
 	}
 }
