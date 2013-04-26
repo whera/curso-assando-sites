a1085689fa36ff98d656e7766051bca7e3db49c2
Ronaldo M. Rodrigues <ronaldo@whera.com.br>
2013-04-26 15:08:03 -0300

    deletando campo descricao do find

--- Model/Palestra.php
+++ Model/Palestra.php
@@ -4,7 +4,7 @@ class Palestra extends AppModel{
 
 	public function setPalestraCidade($strCidade){
 		$params = array(
-			'fields' => array('Palestra.titulo', 'Palestra.data', 'Palestra.descricao', 'Palestrante.nome', 'Palestrante.site'),
+			'fields' => array('Palestra.titulo', 'Palestra.data', 'Palestrante.nome', 'Palestrante.site'),
 			'conditions' => array( 'Palestra.ativo' => 'T', 'Palestra.cidade' => $strCidade)
 		);
 		$sql = $this->find('all', $params);
