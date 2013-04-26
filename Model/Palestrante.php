5e8199cc00cb3755ec9659b486ac0da8cb347c7d
Ronaldo M. Rodrigues <ronaldo@whera.com.br>
2013-04-26 16:10:24 -0300

    Criado metodo ListaPalestrantes responsável por buscar os palastrantes cadastrados no banco de dados

--- Model/Palestrante.php
+++ Model/Palestrante.php
@@ -2,5 +2,21 @@
 App::uses('AppModel', 'Model');
 
 class Palestrante extends AppModel{
+
+	public $order = array('nome' => 'ASC');
+
+	public function ListaPalestrantes(){
+		$params = array(
+			'fields' => array('Palestrante.nome', 'Palestrante.descricao', 'Palestrante.site')
+		);
+		$sql = $this->find('all', $params);
+
+		if(count($sql) > 0){
+			return $sql;
+		}
+		else{
+			return false;
+		}
+	}
 }
 ?>
