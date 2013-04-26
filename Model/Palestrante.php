c382d91dc16f80d796756e379b15ed58f73d740d
Ronaldo M. Rodrigues <ronaldo@whera.com.br>
2013-04-26 16:40:19 -0300

    Método responsável por pegar dados do palestrante

--- Model/Palestrante.php
+++ Model/Palestrante.php
@@ -18,5 +18,22 @@ class Palestrante extends AppModel{
 			return false;
 		}
 	}
+
+
+	public function PegaPalestrante($strPalestrante){
+		$params = array(
+			'fields' = array('Palestrante.nome', 'Palestrante.descricao', 'Palestrante.site'),
+			'conditions' => array( 'Palestrante.slug' => $strPalestrante)
+		);
+
+		$sql = $this->find('all', $params);
+
+		if(count($sql) > 0){
+			return $sql;
+		}
+		else{
+			return false;
+		}
+	}
 }
 ?>
